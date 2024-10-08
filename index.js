e995cd20c1c23424590a3bf5477c903973ce57c6
theajack <1506282385@qq.com>
2024-10-08 19:55:03 +0800

    feat: init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: chenzhongsheng
+ * @Date: 2024-10-08 19:50:06
+ * @Description: Coding something
+ */
+import { Sener, Router } from 'sener';
+
+const router = new Router({
+    '/': ({ responseHtml }) => {
+        return responseHtml('<div>Hello Sener! <a href="/data">(data)</a></div>');
+    },
+    '/data': () => {
+        return {data: 'Hello Sener!'}
+    },
+});
+
+new Sener({
+    port: 9000,
+    middlewares: [router],
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: chenzhongsheng
+ * @Date: 2024-10-08 19:50:14
+ * @Description: Coding something
+-->
+# Super small demo on stackblitz.com
+
+[Visit](https://stackblitz.com/github/sener-js/sener-demo)
